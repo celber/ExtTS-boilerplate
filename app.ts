134b8a852bd79f4ec257928eed5f616b20e10fb1
Dariusz Jakubowski <celber@me.com>
2013-12-03 10:54:23 +0100

    Added skeleton app witch ExtJSTypescript reference

---
+++ app.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by <author> on <date>.
+ * Licence: GPL v2, June 1991
+ */
+
+/// <reference path="reference/extjsTypescript/jsondocs/extjs.d.ts" />
+
+Ext.application({
+  name: "App",
+  autoCreateViewport: true,
+  init: function() {
+    
+  }
+});
---
+++ app/view/Viewport.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by <author> on <date>.
+ * Licence: GPL v2, June 1991
+ */
+ 
+/// <reference path="../../reference/extjsTypescript/jsondocs/extjs.d.ts" />
+
+Ext.define("App.view.Viewport", {
+  
+}, function() {});
